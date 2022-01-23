3f5b30c14e106089e9c2b31009604495389a0541
gcxfd <i@rmw.link>
2022-01-23 15:34:55 +0800

    ⭕️

--- dist.sh
+++ dist.sh
@@ -20,27 +20,6 @@ if ! [ -x "$(command -v xonsh)" ]; then
 pip3 install -r requirements.txt
 fi
 
-if [ ! -f "./pkg/ico/app.icns" ]; then
-./misc/ico.xsh
-fi
-
-git add app
-git add -u
-git commit -m '⭕️' || true
-
-cd pkg/app
-
-npm version patch
-
-VER=`cat package.json| jq '.version' -r`
-
-cd $DIR
-
-cat pkg/app/package.json | jq -c > app/package.json
-git add -u
-git commit -m "v$VER"
-git push
-
 ./pkg/pkg.xsh
 .direnv/bin/coffee ./pkg/upload.coffee
 
