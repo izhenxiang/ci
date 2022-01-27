799e7fcee3eccd42f34b1eb8b7ab62ffdeebcd08
gcxfd <i@rmw.link>
2022-01-27 19:39:50 +0800

    ⭕️

---
+++ app/w/6ca76ff5.js
@@ -0,0 +1,1 @@
+import{h as f,c as r}from"./1629a16e.js";var i=new Proxy(()=>{},{get:(s,o)=>(...t)=>{var e;for(e of f)typeof e[o]=="function"&&e[o](...t);t.unshift(o),r.postMessage(t)}});export{i as b};
