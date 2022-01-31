7abc937d0a57ab895cd47fee8a8d13ef3c412227
gcxfd <i@rmw.link>
2022-01-31 20:28:46 +0800

    ⭕️

--- app/$.js
+++ app/$.js
@@ -1,1 +1,1 @@
-import{k as t,B as r,o as n}from"./b.js";import{_}from"./9.js";var o={setup:()=>{}};function a(e,s){return n(),t("button",null,[r(e.$slots,"default",{},void 0,!0)])}var c=_(o,[["render",a],["__scopeId","data-v-71ed6340"]]);export{c as b};
+import{h as f,c as r}from"./-.js";var i=new Proxy(()=>{},{get:(s,o)=>(...t)=>{var e;for(e of f)typeof e[o]=="function"&&e[o](...t);t.unshift(o),r.postMessage(t)}});export{i as b};
--- app/7.js
+++ app/7.js
@@ -1,1 +1,1 @@
-"use strict";var e=require("electron");(async()=>(await e.app.whenReady(),await Promise.resolve().then(function(){return require("./n.js")})))();
+import{s as r}from"./2.js";var o=r.record({src:"all",area:0});export{o as c};
