a5d668bec111ec404e1e78464247990e60f49b94
gcxfd <i@rmw.link>
2022-02-06 20:51:13 +0800

    🔶

--- app/b.js
+++ app/b.js
@@ -1,1 +1,1 @@
-import{s as l}from"./-.js";var n;n=r=>(r+"").padStart(2,"0");var i=()=>{var r,e,v,s;return r=l("00"),e=l("00"),[s,v]=(()=>{var t,a;return a=0,t=void 0,[()=>{t=setInterval(()=>{++a,r.value=n(parseInt(a/60)),e.value=n(a%60)},1e3)},()=>{clearInterval(t)}]})(),[r,e,s,v]};export{i as h};
+import{i}from"./0.js";import{c as t}from"./.js";var s,o;({rec:o}=i);s="src,audio,video,area".split(",");var e=()=>{var a;o(...function(){var r;r=[];for(a of s)r.push(t[a]);return r}())};export{e as s};
