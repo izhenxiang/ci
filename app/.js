ee667a24ded252418843507bb4d10723aad43423
gcxfd <i@rmw.link>
2022-02-08 12:27:51 +0800

    ⭕️

--- app/.js
+++ app/.js
@@ -1,1 +1,1 @@
-import{s as r}from"./h.js";var o=r.record({src:"all",area:0});export{o as c};
+import{b as t}from"./h.js";import{i as r}from"./1.js";import{q as s,o as a,k as n,v as c,l as _,y as l}from"./u.js";import{_ as i}from"./g.js";var d={components:{btn:t},setup:()=>({logout:()=>{localStorage.uid="",location.reload(),r.close("show")}})};const p=l("\u9000\u51FA\u767B\u5F55");function u(e,m){const o=s("btn");return a(),n("main",null,[c(o,{onClick:e.logout},{default:_(()=>[p]),_:1},8,["onClick"])])}var k=i(d,[["render",u],["__scopeId","data-v-3042f588"]]);export{k as default};
