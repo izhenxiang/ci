3e2fe0ed9970e7955888a08b74b788626134ea9f
gcxfd <i@rmw.link>
2022-02-14 03:22:39 +0800

    ⭕️

--- app/a.js
+++ app/a.js
@@ -1,1 +1,1 @@
-import{s}from"./9.js";var n;n=r=>(r+"").padStart(2,"0");var i=()=>{var r,e,v,l;return r=s("00"),e=s("00"),[l,v]=(()=>{var t,a;return a=0,t=void 0,[()=>{t=setInterval(()=>{++a,r.value=n(parseInt(a/60)),e.value=n(a%60)},1e3)},()=>{clearInterval(t)}]})(),[r,e,l,v]};export{i as h};
+var t="https://testing.izhenxiang.com/";export{t as H};
