5a5c8dbfeecd9b4f043aaec37079cf0c88045769
gcxfd <i@rmw.link>
2022-02-18 16:22:20 +0800

    🔶

--- pkg/mac/sign.js
+++ pkg/mac/sign.js
@@ -2,6 +2,16 @@
 //!/usr/bin/env coffee
 var DIR;
 
+process.env.DEBUG = 'electron-notarize*';
+
+import {
+  appBundleId
+} from './config.mjs';
+
+import {
+  notarize
+} from 'electron-notarize';
+
 import {
   join,
   dirname
@@ -16,16 +26,28 @@ import thisdir from '@rmw/thisdir';
 DIR = thisdir(import.meta);
 
 (async() => {
-  var app, entitlements;
-  [app] = process.argv.slice(2);
+  var app, argv, entitlements, env;
+  ({argv, env} = process);
+  [app] = argv.slice(2);
   entitlements = join(DIR, 'i.plist');
   console.log('sign', app);
   await signAsync({
     app,
     verbose: true,
+    hardenedRuntime: true,
     entitlements,
     'entitlements-inherit': entitlements
   });
   console.log('signed');
+  console.log('开始公证');
+  await notarize({
+    appBundleId,
+    tool: 'notarytool',
+    appPath: app,
+    teamId: env.teamId,
+    appleId: env.appleId,
+    appleIdPassword: env.appleIdPassword
+  });
+  console.log('公证上传完成');
   return process.exit();
 })();
