f141e7d4948482ba48ae0462b21803380de29079
gcxfd <i@rmw.link>
2022-02-20 01:03:22 +0800

    ⭕️

--- app/4.js
+++ app/4.js
@@ -1,1 +1,1 @@
-import{k as t,B as r,o as n}from"./$.js";import{_}from"./e.js";var o={setup:()=>{}};function a(e,s){return n(),t("button",null,[r(e.$slots,"default",{},void 0,!0)])}var c=_(o,[["render",a],["__scopeId","data-v-71ed6340"]]);export{c as b};
+var t=new Proxy({},{get:(i,r)=>a=>{var e;return e={},e[r]={deviceId:{exact:a}},navigator.mediaDevices.getUserMedia(e)}});export{t as m};
