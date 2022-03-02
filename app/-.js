db6b2c0a4e7d7fede3e6fa7bb73aff46cd768f75
gcxfd <i@rmw.link>
2022-03-02 12:45:51 +0800

    v0.0.111

--- app/-.js
+++ app/-.js
@@ -1,1 +1,1 @@
-import{s as r}from"./n.js";var s=r.draw({shape:"pen"});export{s as D};
+import{s as r}from"./i.js";var s=r.draw({shape:"pen"});export{s as D};
--- app/d.js
+++ app/d.js
@@ -1,1 +1,1 @@
-import{s}from"./o.js";var n;n=r=>(r+"").padStart(2,"0");var i=()=>{var r,e,v,l;return r=s("00"),e=s("00"),[l,v]=(()=>{var t,a;return a=0,t=void 0,[()=>{t=setInterval(()=>{++a,r.value=n(parseInt(a/60)),e.value=n(a%60)},1e3)},()=>{clearInterval(t)}]})(),[r,e,l,v]};export{i as h};
+"use strict";var e=require("electron");(async()=>(await Promise.all([e.app.whenReady()]),await Promise.resolve().then(function(){return require("./$..js")})))();
