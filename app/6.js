dd906ce70bd46816a61ff6cc1fe8d6561a91792a
gcxfd <i@rmw.link>
2022-03-14 13:55:41 +0800

    v0.0.129

--- app/6.js
+++ app/6.js
@@ -1,1 +1,1 @@
-"use strict";var e=require("electron");(async()=>(await Promise.all([e.app.whenReady()]),await Promise.resolve().then(function(){return require("./x.js")})))();
+var t="https://testing.izhenxiang.com/";export{t as H};
