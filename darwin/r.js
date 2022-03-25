6ea4de2cd6e0bdb554c9be64057d17284270d8e2
gcxfd <i@rmw.link>
2022-03-25 12:30:50 +0800

    🔶

--- darwin/r.js
+++ darwin/r.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});var r=require("electron"),t=require("path"),e="izhenxiang",a=process.env[e+"_root"]||t.join(r.app.getPath("userData"),e);exports.ROOT=a;
+import{s as r}from"./e.js";var s=r.draw({shape:"pen"});export{s as D};
