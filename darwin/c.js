b738927d4467c8a0b944f58fd9d53ade0f3585bc
gcxfd <i@rmw.link>
2022-04-01 11:07:00 +0800

    🔶

--- darwin/c.js
+++ darwin/c.js
@@ -1,1 +1,1 @@
-"use strict";var e=require("electron");(async()=>(await Promise.all([e.app.whenReady()]),await Promise.resolve().then(function(){return require("./b.js")})))();
+import{d as t,e as r,o as n}from"./x.js";import{_}from"./9.js";var o={setup:()=>{}};function a(e,s){return n(),t("button",null,[r(e.$slots,"default",{},void 0,!0)])}var c=_(o,[["render",a],["__scopeId","data-v-71ed6340"]]);export{c as b};
