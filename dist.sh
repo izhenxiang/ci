5e88519e417e711fd435c530ffe48785755b0437
gcxfd <i@rmw.link>
2022-04-21 11:46:57 +0800

    🔶

--- dist.sh
+++ dist.sh
@@ -4,6 +4,13 @@ DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
 
+if [ ! -d dist ] ; then
+git clone https://e.coding.net/izhenxiang/desk/dist.git --depth=1
+else
+cd dist
+git -C dist pull
+fi
+
 ./build.sh
 
 .direnv/bin/coffee ./pkg/auto_update.coffee
